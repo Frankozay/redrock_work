b47611af7d98759c84797f4ffd590cdb28d1565d
Frankozay <36227547+Frankozay@users.noreply.github.com>
2018-10-31 17:27:08 +0800

    Update test.java

--- src/test.java
+++ src/test.java
@@ -4,7 +4,7 @@ public class test {
     public static void main(String args[]) {
         int choose;
         Scanner input = new Scanner(System.in);
-        System.out.println("输入1为时间戳转时间,输入2为时间转时间戳:");
+        System.out.println("输入1为时间戳转时间,输入2为时间转时间戳,输入3为时差计算:");
         choose = input.nextInt();
         input.nextLine();
         if (choose == 1) {
@@ -15,7 +15,10 @@ public class test {
             System.out.println("请输入日期:");
             TimeStamp timeStamp = new TimeStamp(input.nextLine());
             System.out.println("转换的时间戳为:" + timeStamp.dateToStamptime());
+        } else if (choose == 3) {
+            System.out.println("请输入老日期及新日期:");
+            TimeStamp timeStamp = new TimeStamp(input.nextLine(), input.nextLine());
+            System.out.println("时间相差为:" + timeStamp.timeDifference());
         }
-
     }
 }
