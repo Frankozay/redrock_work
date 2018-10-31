44cfa547fab7fff5bc11cde04560efa77ef32721
Frankozay <36227547+Frankozay@users.noreply.github.com>
2018-10-31 17:26:26 +0800

    Update TimeStamp.java

--- src/TimeStamp.java
+++ src/TimeStamp.java
@@ -4,6 +4,7 @@ public class TimeStamp {
     private long stampTime;
     private int isLeapYear[] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     private int isNotLeapYear[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private String[] dateFirst, dateSecond;
 
     public TimeStamp(String newDate) {
         String[] daysAndTimes = newDate.split("-|\\:|\\s+");
@@ -20,6 +21,11 @@ public class TimeStamp {
         stampTime = newStampTime;
     }
 
+    public TimeStamp(String oldDate, String newDate) {
+        dateFirst = oldDate.split("-|\\:|\\s+");
+        dateSecond = newDate.split("-|\\:|\\s+");
+    }
+
     private boolean isLeap(int year) {
         if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) return true;
         else return false;
@@ -27,7 +33,7 @@ public class TimeStamp {
 
     public long dateToStamptime() { //日期转时间戳。大体思路为先计算今年之前的总天数，再计算本年天数。
         int getStampTime;
-        int countDays = 0;
+        int countDays;
         int countLeapYear = 0;
         int countYear = year - 1970;
         for (int i = 1970; i < year; i++) {
@@ -60,7 +66,7 @@ public class TimeStamp {
         int seconds = countTimes % 60;
         String date;
 
-        hours += 8;//北京时间
+        hours += 8;
         if (hours >= 24) {
             hours -= 24;
             days++;
@@ -79,6 +85,7 @@ public class TimeStamp {
             } else {
                 countDays += 365;
             }
+
         }//特殊情况处理。years本为大体计算的年数，之前计算时当做每年365天，再减去闰年产生的天数可能会产生剩余天数小于0的情况。
         if (isLeap(years)) {
             for (int i = 0; countDays > isLeapYear[i]; i++) {
@@ -113,5 +120,54 @@ public class TimeStamp {
         }//特殊情况，分闰年非闰年处理。days可能会大于本月天数，按情况将其处理。
         date = String.format("%d-%02d-%02d %02d:%02d:%02d", years, months, days, hours, minutes, seconds);
         return date;
+    }
+
+    public long timeDifference() {
+        int yearOld, monthOld, dayOld, hourOld, minuteOld, secondOld;
+        int yearNew, monthNew, dayNew, hourNew, minuteNew, secondNew;
+        yearOld = Integer.parseInt(dateFirst[0]);
+        monthOld = Integer.parseInt(dateFirst[1]);
+        dayOld = Integer.parseInt(dateFirst[2]);
+        hourOld = Integer.parseInt(dateFirst[3]);
+        minuteOld = Integer.parseInt(dateFirst[4]);
+        secondOld = Integer.parseInt(dateFirst[5]);
+        yearNew = Integer.parseInt(dateSecond[0]);
+        monthNew = Integer.parseInt(dateSecond[1]);
+        dayNew = Integer.parseInt(dateSecond[2]);
+        hourNew = Integer.parseInt(dateSecond[3]);
+        minuteNew = Integer.parseInt(dateSecond[4]);
+        secondNew = Integer.parseInt(dateSecond[5]);
+        int countDays;//总天数
+        int countLeapYear = 0;
+        int countYear = yearNew - yearOld - 1;
+        int countSeconds = 0;
+        for (int i = yearOld + 1; i < yearNew; i++) {
+            if (isLeap(i)) countLeapYear++;
+        }//计算闰年数
+        countDays = countYear * 365 + countLeapYear;
+        if (isLeap(yearNew)) {
+            for (int i = 0; i < monthNew - 1; i++) {
+                countDays += isLeapYear[i];
+            }
+        } else {
+            for (int i = 0; i < monthNew - 1; i++) {
+                countDays += isNotLeapYear[i];
+            }
+        }
+        countDays += dayNew - 1;//上述为计算新日期当年第一天到当日天数
+        if (isLeap(yearOld)) {
+            for (int i = monthOld; i < 12; i++) {
+                countDays += isLeapYear[i];
+            }
+            countDays += isLeapYear[monthOld - 1] - dayOld;
+        } else {
+            for (int i = monthOld; i < 12; i++) {
+                countDays += isNotLeapYear[i];
+            }
+            countDays += isNotLeapYear[monthOld - 1] - dayOld;
+        }//计算旧日期到第二年起始天数
+        countSeconds += hourNew * 3600 + minuteNew * 60 + secondNew + (24 - hourOld - 1) * 3600 + (60 - minuteOld - 1) * 60 + 60 - secondOld;
+        countSeconds += countDays * (24 * 60 * 60);
+        return countSeconds;
     }
 }
